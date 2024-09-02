0b3a04c7d3f7dddfa051415c837196a716756bdf
Cesar Rodrigo Rullan <cesarodrullan@gmail.com>
2024-09-02 16:03:04 -0300

    configuracion del modelo wine

--- app/Models/Wine.php
+++ app/Models/Wine.php
@@ -2,10 +2,46 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\HasSlug;
+use App\Traits\ImageUrl;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use NumberFormatter;
 
 class Wine extends Model
 {
-    use HasFactory;
+    use HasSlug;
+    use ImageUrl;
+
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'description',
+        'year',
+        'price',
+        'stock',
+        'image'
+    ];
+
+    protected function casts():array{
+        return [
+            'year' => 'integer',
+            'price' => 'decimal:2',
+            'stock' => 'integer'
+        ];
+    }
+
+    public function category():BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    //formatted_price
+    public function formattedPrice():Attribute{
+        $formatter = new NumberFormatter('es_Ar',NumberFormatter::CURRENCY);
+        return Attribute::make(
+            get : fn () => $formatter->formatCurrency($this->price,'ARS')
+        );
+    }
 }
