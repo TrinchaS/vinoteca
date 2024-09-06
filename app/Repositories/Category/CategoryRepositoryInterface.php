49fe53496eab0f3fe88680b81ce01b894c5dad47
Cesar Rodrigo Rullan <cesarodrullan@gmail.com>
2024-09-06 15:32:09 -0300

    Trait para operaciones de CRUD reutilizables

--- app/Repositories/Category/CategoryRepositoryInterface.php
+++ app/Repositories/Category/CategoryRepositoryInterface.php
@@ -6,7 +6,7 @@ use App\Models\Category;
 
 interface CategoryRepositoryInterface
 {
-    //
+    //nos devuelve una instancia vacia del modelo o si el slug no es null no devuelve una instancia del modelo que encontro en la base de dato
     public function model(?string $slug = null): Category;
 
     public function paginate(array $counts = [], array $relationships = [], int $perPage = 10);
