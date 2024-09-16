1cffb25e43a13439e28c53dc2c77922a48f63435
Cesar Rodrigo Rullan <cesarodrullan@gmail.com>
2024-09-16 17:04:24 -0300

    Añadiendo validación Unique para alta-edición de categorías

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class CategoryRequest extends FormRequest
 {
@@ -26,7 +27,7 @@ class CategoryRequest extends FormRequest
             $imageRules = 'required | image | mimes:jpeg,jpg,png | max:2048';
         }
         return [
-            'name' => 'required | string | max:255',
+            'name' => ['required','string','max:255', Rule::unique('categories','name')->ignore($this->route('category'))],
             'description' => 'required | string | max:2000',
             'image' => $imageRules
         ];
@@ -37,6 +38,7 @@ class CategoryRequest extends FormRequest
             'name.required' => 'La categoría es requerida.',
             'name.string' => 'La categoría debe ser un texto.',
             'name.max' => 'La categoría no debe exceder los :max caracteres.',
+            'name.unique' => 'La categoría ya existe.',
             'description.required' => 'La descripción es requerida.',
             'description.string' => 'La descripción debe ser un texto.',
             'description.max' => 'La descripción no debe exceder los :max caracteres.',
